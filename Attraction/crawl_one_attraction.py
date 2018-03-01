78bf3c1ee0a48dc018b0c43c49eda83848015a5c
MShi <2017shim@gmail.com>
2018-03-01 01:17:21 -0600

    update coa, add begin_num

--- Attraction/crawl_one_attraction.py
+++ Attraction/crawl_one_attraction.py
@@ -4,7 +4,7 @@ import click_03
 
 
 
-def crawl_attr(attraction_file):
+def crawl_attr(attraction_file, begin_num):
     '''
     crawler attributes and reviews of attractions in the given file
     Inputs:
@@ -19,13 +19,14 @@ def crawl_attr(attraction_file):
             attr_name.append(row[0])
             url.append(row[1])
 
-    for i in range(len(url)):
+    for i in range(begin_num, len(url)):
         name = attr_name[i]
         print(name)
         c = click_03.ChromeDriver(url[i])
         is_attraction = c.is_attraction()
         attr = json.loads(is_attraction.get_attribute('text'))
         c.add_reviews(attr)
+        print(len(attr['reviews']))
         attr_dict = {}
         attr_dict[name] = attr
         with open('{}.json'.format(name), 'w') as file:
