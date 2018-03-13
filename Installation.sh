f136f75a766d9e1e37047c24f86511f78f75a4aa
ivy chen <ivychen@ivys-MacBook-Pro.local>
2018-03-13 13:15:31 -0500

    add shell

---
+++ Installation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash  
+ 
+# Bash script for installing most required packages for AldaCourse.
+sudo apt-get clean
+sudo apt-get update
+sudo python3 -m pip install Django
+sudo python3 -m pip install selenium
+sudo python3 -m pip install numpy
+
